11e921403c09c128a96e9a1ee54f03b9c15c4877
tanacchi <q111026d@mail.kyutech.jp>
2019-12-11 03:51:46 +0900

    Add codes of level 10

---
+++ ltx-7/lt7/lv10-2.py
@@ -0,0 +1,44 @@
+from reader import read
+
+
+dispatch_table = {
+    '+': lambda *args: args[0] + args[1],
+    '-': lambda *args: args[0] - args[1]
+}
+
+def lambda_content(vargs, args, exprs):  # Added
+    for varg, arg in zip(vargs, args):
+        dispatch_table[varg] = arg
+    return_value = None
+    for expr in exprs:
+        return_value = evaluate(expr)
+    return return_value
+
+def evaluate(src):
+    if isinstance(src, str):
+        return dispatch_table[src]
+    elif not isinstance(src, list):
+        return src
+    elif src[0] == 'define':
+        key, value = src[1], evaluate(src[2])
+        dispatch_table[key] = value
+    elif src[0] == 'lambda':
+        vargs, exprs = src[1], src[2:]
+        return lambda *args: lambda_content(vargs, args, exprs)  # Modified
+    else:
+        operator = evaluate(src[0])
+        args = [evaluate(arg) for arg in src[1:]]
+        return operator(*args)
+
+
+######## main ########
+
+print("Welcome to lt7 !!\n")
+
+while True:
+    result = evaluate(read())
+
+    print("-"*10)
+    print("  {}  <{}>".format(result, type(result).__name__))
+    print("-"*10)
+    print()
