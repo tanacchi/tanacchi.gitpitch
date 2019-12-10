f803e5320a711c02035c1f41f3523fea830ffb5e
tanacchi <q111026d@mail.kyutech.jp>
2019-12-10 17:44:08 +0900

    Add code of level4

---
+++ ltx-7/lt7/lv04.py
@@ -0,0 +1,36 @@
+def tokenize(input_str):
+    try:
+        return int(input_str)
+    except:
+        return input_str
+
+
+def parse(input_str):
+    result = []
+    new_token = ""
+    for ch in input_str:
+        if ch == ' ':
+            pass
+        elif ch == '(':
+            pass
+        elif ch == ')':
+            pass
+        else:
+            new_token += ch
+    else:
+        result.append(new_token)
+        return result
+
+
+######## main ########
+
+print("Welcome to lt7 !!\n")
+
+while True:
+    input_str = input("> ")
+    src = parse(input_str) # Modified
+
+    print("-"*10)
+    print("  {} ({})".format(src, type(src)))  # Modified
+    print("-"*10)
+    print()
