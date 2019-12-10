5d6d3f0a250486e523772b5b5e005425bc67df93
tanacchi <q111026d@mail.kyutech.jp>
2019-12-10 17:21:30 +0900

    Add code of lt7

---
+++ ltx-7/lt7/reader.py
@@ -0,0 +1,50 @@
+import re
+
+def __tokenize(target):
+    try: 
+        return int(target)
+    except ValueError:
+        try:
+            return float(target)
+        except:
+            return target
+
+def __make_list(src, index):
+    result = []
+    while src[index:]:
+        match = re.search(re.compile(r'^[^ ()\n]+'), src[index:])
+        if match != None:
+            detected_token = match.group(0)
+            index += len(detected_token)
+            result.append(__tokenize(detected_token))
+        elif src[index] == "(":
+            sub_result = __make_list(src, index + 1)
+            result.append(sub_result['list'])
+            index = sub_result['index']
+        elif src[index] == ")":
+            break
+        else:
+            index += 1
+    return {'list': result, 'index': index + 1}
+
+def parse(src):
+    parse_result= []
+    index = 0
+    while src[index:]:
+        buff = __make_list(src[index:], 0)
+        parse_result.append(buff['list'])
+        index = buff['index']
+    return parse_result[0][0]  # TODO: refactor
+
+def read():
+    source_str = ""
+    while True:
+        source_str += input("> ") + " "
+        depth = source_str.count("(") - source_str.count(")")
+        if depth > 0:
+            print(">>" * depth, end="")
+        elif source_str.strip() is "":
+            continue
+        else:
+            break
+    return parse(source_str)
