eecfa087687f293993bccae352d574dbc65ddbbd
tanacchi <q111026d@mail.kyutech.jp>
2019-12-10 17:29:54 +0900

    Add code of level1 and 2

---
+++ ltx-7/lt7/lv02.py
@@ -0,0 +1,9 @@
+print("Welcome to lt7 !!\n")
+
+while True:
+    input_str = input("> ")
+
+    print("-"*10)
+    print("  {} ({})".format(input_str, type(input_str)))
+    print("-"*10)
+    print()
